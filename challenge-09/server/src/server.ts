a01609708f3408408e07d0a6f45280bb0bb6db45
Liam Rayback <liam@macbook.local>
2024-10-21 20:38:15 -0600

    updates on server.ts

--- challenge-09/server/src/server.ts
+++ challenge-09/server/src/server.ts
@@ -9,12 +9,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
-
-// TODO: Implement middleware for parsing JSON and urlencoded form data
-
-// TODO: Implement middleware to connect the routes
-app.use(routes);
-
-// Start the server on the port
+// Serve static files
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+// Middleware for parsing JSON
+app.use(express.json());
+// Connect the routes
+app.use('/api/weather/weatherRoutes.ts', routes);
+// Start the server
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+
+// import dotenv from 'dotenv';
+// import express from 'express';
+// import path from 'path';
+// import routes from './routes/index.js';
+// dotenv.config();
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// // Serve static files
+// app.use(express.static(path.join(__dirname, '../../client/dist')));
+// // Middleware for parsing JSON
+// app.use(express.json());
+// // Connect the routes
+// app.use('/api/weather/weatherRoutes.ts', routes);
+// // Start the server
+// app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
